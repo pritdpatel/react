81bd296d4311d18e8341e84520419818bba43bea
pritdpatel <ppd2852@gmail.com>
2023-09-13 04:59:09 -0400

    react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import Form from './include/JS/forms';   
-import SqliteData from './include/JS/SqliteData';                                                                                      
-export default class App extends React.Component {
-
-  render(){
-    return (
-    <SqliteData/>
-    );
+import React from "react";
+import "./App.css";
+import { Path, Links_name } from './include/data_set/Data';
+class App extends React.Component{
+  App(props){
+    this.props=props
   }
+  render (){
 
+    
+    return (<div>
+      <header className="main_header">
+        <h1>Dometell</h1>
+    
+        <links className="list_of_link">
+          <a className="element" href="@">{Links_name['id.name']}</a>
+          <a className="element" href="!">python Projects</a>
+          <a className="element" href="#">Java projects</a>
+        </links>  
+      </header>
+      <main id="main">
+        <p>{Links_name['id']['number']}</p>
+      </main>
+    </div>);
+  }
 }
+
+export default App;
---
+++ src/include/data_set/Data.js
@@ -0,0 +1,14 @@
+var Path={
+    name:"prit",
+    id:{
+        section:1,
+        class:'B',
+    }
+};
+var Links_name = {
+    id:{number:1,'name':'home'},
+    id:{number:2,'name':'python Projects'},
+    id:{number:3,'name':'Java Projects'},
+
+};
+export { Path, Links_name };
